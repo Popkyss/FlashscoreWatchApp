41196f290193dfca7d651dbfbb4a5f718a5ef897
Jan Pokorný <jan.pokorny@livesport.eu>
2024-01-05 13:05:21 +0100

    Initial Commit

---
+++ FlashscoreWatchApp Watch App/FlashscoreWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashscoreWatchAppApp.swift
+//  FlashscoreWatchApp Watch App
+//
+//  Created by Jan Pokorný on 05.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashscoreWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlashscoreWatchApp/FlashscoreWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashscoreWatchAppApp.swift
+//  FlashscoreWatchApp
+//
+//  Created by Jan Pokorný on 05.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashscoreWatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
